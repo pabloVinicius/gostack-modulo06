1060b52b171fc5d8cb9d429fa74b9e22bd35a22f
Pablo Vinícius <vinicius.pablo.18@gmail.com>
2019-12-14 19:42:20 -0300

    adding pagination and refresh control

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
-
 import {
   Container,
   Header,
@@ -18,19 +17,44 @@ import {
 } from './styles';
 
 const User = ({ navigation }) => {
-  const user = navigation.getParam('user');
   const [loading, changeLoading] = useState(true);
   const [stars, changeStars] = useState([]);
+  const [page, changePage] = useState(1);
+  const [stopRequest, changeStopRequest] = useState(false);
+  const user = navigation.getParam('user');
+
+  const loadData = () => {
+    if (!stopRequest) {
+      api
+        .get(`/users/${user.login}/starred`, {
+          params: {
+            page,
+          },
+        })
+        .then(response => {
+          changeStars([...stars, ...response.data]);
+          changeLoading(false);
+        })
+        .catch(() => {
+          changeLoading(false);
+          changeStopRequest(true);
+        });
+    }
+  };
 
   useEffect(() => {
-    api
-      .get(`/users/${user.login}/starred`)
-      .then(response => {
-        changeStars(response.data);
-        changeLoading(false);
-      })
-      .catch(() => changeLoading(false));
+    loadData();
   }, []);
+
+  useEffect(() => {
+    loadData();
+  }, [page]);
+
+  const handleRefresh = () => {
+    changeLoading(true);
+    changePage(0);
+    changeStars([]);
+  };
 
   return (
     <Container>
@@ -48,7 +72,11 @@ const User = ({ navigation }) => {
       ) : (
         <Stars
           data={stars}
+          onRefresh={handleRefresh}
+          refreshing={loading}
           keyExtractor={star => `${star.id}`}
+          onEndReachedThreshold={0.2}
+          onEndReached={() => changePage(page + 1)}
           renderItem={({ item }) => (
             <Starred>
               <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
