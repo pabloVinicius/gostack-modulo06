361474c1e06ec7ad0bbc43dde7ba3160dd309130
Pablo Vinícius <vinicius.pablo.18@gmail.com>
2019-12-14 19:16:34 -0300

    implementing stars view

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,19 +1,77 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator } from 'react-native';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
-const User = props => {
-  console.tron.log('user', props);
+const User = ({ navigation }) => {
+  const user = navigation.getParam('user');
+  const [loading, changeLoading] = useState(true);
+  const [stars, changeStars] = useState([]);
+
+  useEffect(() => {
+    api
+      .get(`/users/${user.login}/starred`)
+      .then(response => {
+        changeStars(response.data);
+        changeLoading(false);
+      })
+      .catch(() => changeLoading(false));
+  }, []);
+
   return (
-    <View>
-      <Text>User</Text>
-    </View>
+    <Container>
+      <Header>
+        <Avatar source={{ uri: user.avatar }} />
+        <Name>{user.name}</Name>
+        <Bio>{user.bio}</Bio>
+      </Header>
+      {loading ? (
+        <ActivityIndicator
+          size="large"
+          style={{ marginTop: 20 }}
+          color="#7159c1"
+        />
+      ) : (
+        <Stars
+          data={stars}
+          keyExtractor={star => `${star.id}`}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      )}
+    </Container>
   );
 };
 
-User.navigationOptions = {
-  title: 'User',
+User.navigationOptions = ({ navigation }) => ({
+  title: navigation.getParam('user').name,
+});
+
+User.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+  }).isRequired,
 };
 
 export default User;
